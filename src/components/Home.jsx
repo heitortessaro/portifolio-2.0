1b846921696cc0268d6201bcf4b8aa8b91926c6c
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-09 17:43:32 -0300

    Populate About component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,32 @@
-import React from "react";
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import React from 'react';
+import {HiArrowNarrowRight} from 'react-icons/hi';
 
 export default function Home() {
   return (
-    <div name='home' className="w-full h-screen bg-[#0a192f]">
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600 ">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Heitor José Tessaro</h1>
+          Heitor José Tessaro
+        </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.</h2>
+          I'm a Full Stack Developer.
+        </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          Oi! Sou mestre em engenharia elétrica e atualmente trabalho como technical writer. No momento estou finalizando o módulo de front end do curso de desenvolvedor web full stack na Trybe. </p>
+          Oi! Sou mestre em engenharia elétrica e atualmente trabalho como
+          technical writer. No momento estou finalizando o módulo de front end
+          do curso de desenvolvedor web full stack na Trybe.{' '}
+        </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600"> 
-          View my work! 
-          <span className="group-hover:rotate-90 duration-300">
-            <HiArrowNarrowRight className="ml-3"/> 
-          </span>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View my work!
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
           </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
