534d3aad3e42db3ad44144d2fc54fcd2351d6712
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-10 16:08:34 -0300

    Update about section and skills section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,19 +14,12 @@ export default function About() {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
-            <p>Hi. I'm Heitor, nice to meet you. Please take a look around.</p>
+            <p>Hi. I'm Heitor. Nice to meet you! Please, take a look around.</p>
           </div>
           <div>
-            <p>I am passionate about learning and today, I'm trying to positively impact people's lives. </p>
-            <p>I have a master's degree in electrical engineering and work as a technical writer nowadays. I just finished the front-end module, and I'm currently starting the front-end module of the full-stack web developer course at Trybe.
-            <p>Previously, I worked for 04 years as a researcher at two federal universities and 02 years as a college professor in Brazil. Furthermore, I was also a member of AIESEC during an exchange program at Arizona State University.</p>
-
-              {/* I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips? */}
-            </p>
+            <p className='pb-2'>I'm passionate about learning and creating impactful and inspiring experiences in people's lives. </p>
+            <p className='pb-2'>I'm a web developer and technical writer with an electrical engineering background. Previously, I worked as a college professor and researcher for six years.</p>
+            <p className='pb-2 font-bold'>Let me help you remove the roadblock holding your success?</p>
           </div>
         </div>
       </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,12 +4,12 @@ import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
-import FireBase from '../assets/firebase.png';
-import AWS from '../assets/aws.png';
+// import Node from '../assets/node.png';
+// import FireBase from '../assets/firebase.png';
+// import AWS from '../assets/aws.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
+// import Mongo from '../assets/mongo.png';
 
 export default function Skills() {
   return (
@@ -18,10 +18,10 @@ export default function Skills() {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-              <p className='py-4'>// These are the technologies I've worked with</p>
+              <p className='py-4'>These are the technologies I've worked with recently</p>
           </div>
 
-          <div className='w-full grid grid-cols-3 sm:grid-cols-5 gap-4 text-center py-8'>
+          <div className='w-full grid grid-cols-3 md:grid-cols-5 gap-4 text-center py-8'>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                   <p className='my-4'>HTML</p>
@@ -39,9 +39,35 @@ export default function Skills() {
                   <p className='my-4'>REACT</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg' alt="React icon" />
+                  <p className='my-4'>REDUX</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/jest/jest-plain.svg' alt="Jest icon" />
+                  <p className='my-4'>JEST</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg' alt="Bootstrap icon" />
+                  <p className='my-4'>BOOTSTRAP</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
+                  <p className='my-4'>TAILWIND</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={GitHub} alt="Github icon" />
                   <p className='my-4'>GITHUB</p>
               </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg' alt="Python icon" />
+                  <p className='my-4'>PYTHON</p>
+              </div>
+              {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src='https://camo.githubusercontent.com/e045b6c483968b0b0e13d4dafcbf84237a569e5fb7f5fe6c0a48c48a137e8da7/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6d61746c61622f6d61746c61622d6f726967696e616c2e737667' alt="Matlab icon" />
+                  <p className='my-4'>MATLAB</p>
+              </div> */}
+
+              
               {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
                   <p className='my-4'>NODE JS</p>
