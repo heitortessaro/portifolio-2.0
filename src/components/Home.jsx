9ad438ac32ae167b17216357cdcbcf3a214cee58
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-05-10 15:32:37 -0300

    Update home component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,19 +8,16 @@ export default function Home() {
       {/* container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600 ">Hi, my name is</p>
+        <p className="text-3xl sm:text-5xl font-bold text-[#8892b0]">Heitor José Tessaro.</p>
+        <p className="text-[#8892b0]">I'm a Full Stack Developer.</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Heitor José Tessaro
-        </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.
-        </h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
-          Oi! Sou mestre em engenharia elétrica e atualmente trabalho como
-          technical writer. No momento estou finalizando o módulo de front end
-          do curso de desenvolvedor web full stack na Trybe.{' '}
-        </p>
+          Let's Build Great Experiences Together!
+        </h1>        
+        {/* <p className="text-[#8892b0] py-4 max-w-[700px]">
+          Description
+        </p> */}
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+          <button className="text-white group border-2 px-6 py-3 my-12 flex items-center hover:bg-pink-600 hover:border-pink-600">
             View my work!
             {/* <span className="group-hover:rotate-90 duration-300"> */}
             <span>
