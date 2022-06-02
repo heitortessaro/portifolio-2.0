0d1b95b6fb8466388fcacff2c74371edf0fef18b
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-02 08:51:44 -0300

    Create service with all skills.

---
+++ src/services/skills.js
@@ -0,0 +1,52 @@
+const skills = [
+  {
+    skill:'HTML',
+    img:'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+  },
+  {
+    skill:'CSS',
+    img:'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg',
+  },
+  {
+    skill:'JAVASCRIPT',
+    img:'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  },
+  {
+    skill:'REACT',
+    img:'https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg',
+  },
+  {
+    skill:'REDUX',
+    img:'https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg',
+  },
+  {
+    skill:'JEST',
+    img:'https://raw.githubusercontent.com/devicons/devicon/master/icons/jest/jest-plain.svg',
+  },
+  {
+    skill:'BOOTSTRAP',
+    img:'https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg',
+  },
+  {
+    skill:'TAILWIND',
+    img:'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg',
+  },
+  {
+    skill:'GITHUB',
+    img:'https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg',
+  },
+  {
+    skill:'PYTHON',
+    img:'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+  },
+  {
+    skill:'DOCKER',
+    img:'https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg',
+  },
+  {
+    skill:'MYSQL',
+    img:'https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg',
+  },
+];
+
+export default skills;
