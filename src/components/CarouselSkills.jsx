90f76712d527b7a7459e855ba0f1d3cb25a90819
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-02 09:32:11 -0300

    Refactor CarouselSkills using SkillCard.

--- src/components/CarouselSkills.jsx
+++ src/components/CarouselSkills.jsx
@@ -1,15 +1,4 @@
 import React from 'react';
-
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-// import Node from '../assets/node.png';
-// import FireBase from '../assets/firebase.png';
-// import AWS from '../assets/aws.png';
-import GitHub from '../assets/github.png';
-import Tailwind from '../assets/tailwind.png';
-// import Mongo from '../assets/mongo.png';
 import skills from '../services/skills'
 import SkillCard from './skills/SkillCard';
 
@@ -24,152 +13,26 @@ export default function CarouselSkills() {
         </div>
 
         <div className="carousel carousel-center w-full sm:hidden ">
-          <div id="slide1" className="carousel-item w-full">
-            <div className="w-full grid grid-cols-2 gap-4 text-center py-8">
-              <div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                <p className='my-4'>HTML</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
-                <p className='my-4'>CSS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={JavaScript} alt="JS icon" />
-                <p className='my-4'>JAVASCRIPT</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
-                <p className='my-4'>REACT</p>
-              </div>
-            </div>
-            {/* <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide3" className="btn btn-circle">❮</a>
-              <a href="#slide2" className="btn btn-circle">❯</a>
-            </div> */}
-          </div>
-          <div id="slide2" className="carousel-item w-full">
-            <div className="w-full grid grid-cols-2 gap-4 text-center py-8">
-              <div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg' alt="React icon" />
-                <p className='my-4'>REDUX</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/jest/jest-plain.svg' alt="Jest icon" />
-                <p className='my-4'>JEST</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg' alt="Bootstrap icon" />
-                <p className='my-4'>BOOTSTRAP</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
-                <p className='my-4'>TAILWIND</p>
-              </div>
-            </div>
-            {/* <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide1" className="btn btn-circle">❮</a>
-              <a href="#slide3" className="btn btn-circle">❯</a>
-            </div> */}
-          </div>
-          <div id="slide3" className="carousel-item  w-full">
-            <div className="w-full grid grid-cols-2 gap-4 text-center py-8">
-              <div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src={GitHub} alt="Github icon" />
-                <p className='my-4'>GITHUB</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg' alt="Python icon" />
-                <p className='my-4'>PYTHON</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg' alt="Python icon" />
-                <p className='my-4'>DOCKER</p>
-              </div><div className='shadow-md shadow-[#040c16] '>
-                <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg' alt="Python icon" />
-                <p className='my-4'>MYSQL</p>
-              </div>
-            </div>
-            {/* <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide2" className="btn btn-circle">❮</a>
-              <a href="#slide1" className="btn btn-circle">❯</a>
-            </div> */}
-          </div>
-
-
+          {skills.map((e, i, a) => {
+            return (
+              !((i + 1) % 4) && (
+                <div className="carousel-item w-full">
+                  <div className="w-full grid grid-cols-2 gap-4 text-center py-8">
+                    <SkillCard skill={a[i - 3].skill} img={a[i - 3].img} key={a[i - 3].skill + i} />
+                    <SkillCard skill={a[i - 2].skill} img={a[i - 2].img} key={a[i - 2].skill + i} />
+                    <SkillCard skill={a[i - 1].skill} img={a[i - 1].img} key={a[i - 1].skill + i} />
+                    <SkillCard skill={e.skill} img={e.img} key={e.skill + i} />
+                  </div>
+                </div>
+              )
+            )
+          })}
         </div>
 
-
         <div className='hidden w-full sm:grid sm:grid-cols-3 md:grid-cols-6 gap-4 text-center py-8'>
-          {skills.map((e,i) => (
+          {skills.map((e, i) => (
             <SkillCard skill={e.skill} img={e.img} key={e.skill + i} />
           ))}
-          {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-            <p className='my-4'>HTML</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
-            <p className='my-4'>CSS</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={JavaScript} alt="JS icon" />
-            <p className='my-4'>JAVASCRIPT</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
-            <p className='my-4'>REACT</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg' alt="React icon" />
-            <p className='my-4'>REDUX</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/jest/jest-plain.svg' alt="Jest icon" />
-            <p className='my-4'>JEST</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg' alt="Bootstrap icon" />
-            <p className='my-4'>BOOTSTRAP</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
-            <p className='my-4'>TAILWIND</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={GitHub} alt="Github icon" />
-            <p className='my-4'>GITHUB</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg' alt="Python icon" />
-            <p className='my-4'>PYTHON</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg' alt="Python icon" />
-            <p className='my-4'>DOCKER</p>
-          </div><div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src='https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg' alt="Python icon" />
-            <p className='my-4'>MYSQL</p>
-          </div> */}
-
-          {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src='https://camo.githubusercontent.com/e045b6c483968b0b0e13d4dafcbf84237a569e5fb7f5fe6c0a48c48a137e8da7/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6d61746c61622f6d61746c61622d6f726967696e616c2e737667' alt="Matlab icon" />
-                  <p className='my-4'>MATLAB</p>
-              </div> */}
-
-
-          {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
-                  <p className='my-4'>NODE JS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
-                  <p className='my-4'>MONGO DB</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={AWS} alt="HTML icon" />
-                  <p className='my-4'>AWS</p>
-              </div> */}
         </div>
       </div>
     </div>
