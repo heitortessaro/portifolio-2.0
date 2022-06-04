8ce28d1a750d8f98125dbf9d6d85a38201134f48
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-04 15:14:46 -0300

    Fix margins and paddings for sm screen.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,17 +6,17 @@ import {HiOutlineMail} from 'react-icons/hi';
 export default function Contact() {
   return (
     <div name='contact' className='w-full h-screen flex flex-col justify-center items-center mt-8 p-4 bg-main-body'>
-      <div className='w-full sm:w-[600px] pb-8'>
+      <div className='w-full sm:w-[600px] pb-2 sm:pb-8'>
         <p className='text-4xl text-left font-bold inline border-b-4 border-pink-600 text-gray-300'>Hire me</p>
         <p className='text-gray-300 py-4'>Contact me using the form or my social networks</p>
       </div>
       <form method='POST' action="https://getform.io/f/6920c939-1c12-46ff-baf7-ffa20dad9814" className='flex flex-col max-w-[600px] w-full'>
         <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
         <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-        <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 mt-8 mx-auto flex items-center'>Let's Collaborate</button>
+        <textarea className='bg-[#ccd6f6] p-2' name="message" rows="5" placeholder='Message'></textarea>
+        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-2 sm:py-3 mt-4 sm:mt-8 mx-auto flex items-center'>Let's Collaborate</button>
       </form>
-      <ul className='w-full flex justify-center space-x-8 mt-12'>
+      <ul className='w-full flex justify-center space-x-8 mt-4 sm:mt-12'>
         <li className="w-[60px] h-[60px] rounded-full flex justify-between items-center bg-blue-600">
           <a
             className="flex justify-between items-center w-full text-gray-300"
