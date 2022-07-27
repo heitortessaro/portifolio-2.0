1cfac29364d35db1d080de5404c7f5b57b24fb44
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-26 21:47:00 -0300

    Updates projects description.

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,17 +1,19 @@
 const projects = [
   {
     project:'Trivia Game',
-    img:'trivia.png',
-    description: '[FrontEnd] It is a React app of the popular game Trivia. It uses Redux, Hooks, Tailwind, DaisyUI, the Open Trivia Database.',
+    img:'trivia',
+    description: 'Online version of the popular game Trivia. It uses Redux, Hooks, Tailwind, DaisyUI, the Open Trivia Database.',
     demo: 'https://trivia-game-p8t5vpu0f-heitortessaro.vercel.app/',
     code: 'https://github.com/heitortessaro/TriviaGame2/',
+    type: "Front-End",
   },
   {
     project:'Trybe Wallet',
     img:'wallet',
-    description: '[FrontEnd] It provides a currency converter spending control wallet. It uses React, Redux, and Bootstrap.',
+    description: 'Provides a currency converter spending control wallet. It uses React, Redux, and Bootstrap.',
     demo: 'https://trybewallet-ioqsphljg-heitortessaro.vercel.app/',
     code: 'https://github.com/heitortessaro/TrybeWallet',
+    type: "Front-End",
   },
 ];
 
